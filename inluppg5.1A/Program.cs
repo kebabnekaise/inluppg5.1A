e9423148ca83447899d38e73d87fb920fc28d632
emsi06001 <emsi06001@E-PW0234N3>
2023-11-22 12:44:15 +0100

    Add project files.

---
+++ inluppg5.1A/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace inluppg5_2A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ord = new string[] { "Hej", "på", "dig", "din", "filur" };
+            for (int i = 0; i < ord.Length; i++)
+            {
+                Console.WriteLine(ord[i]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
